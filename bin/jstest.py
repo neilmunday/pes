7c26b7b2413a553a70ea51f0786fa0f12ba409f7
Neil Munday <neilmunday@users.noreply.github.com>
2014-12-04 00:03:14 +0000

    Added axis detection for jstest.py

--- bin/jstest.py
+++ bin/jstest.py
@@ -22,11 +22,28 @@
 #    along with PES.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+#
+#    Helper program for testing joysticks with PyGame by Neil Munday
+#
+
 import sys
+import signal
+from signal import SIGTERM
 import pygame
 from pygame.locals import *
 
+def stopTest(sig, dummy):
+	print "Exiting..."
+	sys.exit(0)	
+
 if __name__ == "__main__":
+
+	AXIS_PRESSED = 1
+	AXIS_RELEASED = 2
+
+	signal.signal(signal.SIGTERM, stopTest)
+	signal.signal(signal.SIGINT, stopTest)
+
 	pygame.init()
 	pygame.joystick.init()
 	joystickTotal = pygame.joystick.get_count()
@@ -36,4 +53,43 @@ if __name__ == "__main__":
 		js.init()
 		print "Joystick %d: %s" % (i, js.get_name())
 	
+	print "Listening for joystick events..."
+
+	stop = False
+
+	axisHistory = {}
+
+	while stop == False:
+
+		for event in pygame.event.get():
+			if event.type == pygame.JOYBUTTONDOWN:
+				print "joystick %d, button pressed: %d" % (event.joy, event.button)
+			elif event.type == pygame.JOYBUTTONUP:
+				print "joystick %d, button released: %d" % (event.joy, event.button)
+			elif event.type == pygame.JOYAXISMOTION:
+				if event.value >= 0.5 or event.value <= -0.5:
+					print "joystick %d, axis: %d, value: %f" % (event.joy, event.axis, event.value)
+					if not axisHistory.has_key(event.joy):
+						axisHistory[event.joy] = {}
+
+					if not axisHistory[event.joy].has_key(event.axis):
+						axisHistory[event.joy][event.axis] = AXIS_PRESSED
+					
+					if axisHistory[event.joy][event.axis] == AXIS_RELEASED:
+						axisHistory[event.joy][event.axis] = AXIS_PRESSED
+						print "joystick %d, axis: %d ACTIVATED" % (event.joy, event.axis)
+				else:
+					if not axisHistory.has_key(event.joy):
+						axisHistory[event.joy] = {}
+
+					if not axisHistory[event.joy].has_key(event.axis):
+						axisHistory[event.joy][event.axis] = AXIS_RELEASED
+
+					if axisHistory[event.joy][event.axis] == AXIS_PRESSED:
+						axisHistory[event.joy][event.axis] = AXIS_RELEASED
+						print "joystick %d, axis: %d DEACTIVATED" % (event.joy, event.axis)
+
+		
+	print "Exiting..."
+	sys.exit(0)
 
