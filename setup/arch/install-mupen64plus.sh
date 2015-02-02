08f2c503ca98c5207abd267f2b1b384de6f192a5
Neil Munday <neilmunday@users.noreply.github.com>
2015-02-02 23:47:03 +0000

    Added generation of launcher script.

--- setup/arch/install-mupen64plus.sh
+++ setup/arch/install-mupen64plus.sh
@@ -207,3 +207,13 @@ checkDir projects/unix
 cd projects/unix
 run make clean
 run make SDL_CFLAGS="$SDL_CFLAGS" SDL_LDLIBS="$SDL_LDLIBS" PREFIX=$PREFIX V=1 install
+
+#
+# launcher script
+#
+
+launchScript="$PREFIX/bin/mupen64plus-launcher.sh"
+
+run echo "#!/bin/bash" > $launchScript
+run echo "$PREFIX/bin/mupen64plus --corelib $PREFIX/lib/libmupen64plus.so.2 --datadir $PREFIX/share/mupen64plus --plugindir $PREFIX/lib/mupen64plus --configdir $baseDir/conf.d/mupen64plus \"\\$1\"" >> $launchScript
+
