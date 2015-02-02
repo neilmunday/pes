5dd8e9d05229add193611b125752721ecfa1d655
Neil Munday <neilmunday@users.noreply.github.com>
2015-02-02 23:30:51 +0000

    Added launcher script for mupen64plus

---
+++ setup/arch/mupen64plus-launcher.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/home/pi/pes/emulators/mupen64plus/bin/mupen64plus --corelib /home/pi/pes/emulators/mupen64plus/lib/libmupen64plus.so.2 --datadir /home/pi/pes/emulators/mupen64plus/share/mupen64plus --plugindir /home/pi/pes/emulators/mupen64plus/lib/mupen64plus --configdir /home/pi/pes/conf.d/mupen64plus "$1" 
+
