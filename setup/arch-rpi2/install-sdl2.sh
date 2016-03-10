d1319737b56bd9ab17b46c4ac28a6069fc204dd1
Neil Munday <neilmunday@users.noreply.github.com>
2016-03-10 23:55:31 +0000

    Finished SDL2 install script.

--- setup/arch-rpi2/install-sdl2.sh
+++ setup/arch-rpi2/install-sdl2.sh
@@ -40,63 +40,63 @@ prefix=/opt/sdl2/$VERSION
 export CFLAGS="-mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard"
 export CXXFLAGS="-mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard"
 
-# header "Downloading SDL2"
-# 
-# run cd $srcDir
-# 
-# if [ ! -e $sdl2Tar ]; then
-# 	echo "Downloading SDL2-${VERSION}..."
-# 	run wget http://www.libsdl.org/release/SDL2-${VERSION}.tar.gz
-# fi
-# 
-# checkFile $sdl2Tar
-# 
-# if [ -e $sdl2Dir ]; then
-# 	echo "Removing previously used source..."
-# 	run rm -rvf $sdl2Dir
-# fi
-# run cd $buildDir
-# run tar xvfz $sdl2Tar
-# checkDir $sdl2Dir
-# run cd $sdl2Dir
-# 
-# ./configure --prefix=$prefix --host=arm-raspberry-linux-gnueabihf --disable-video-opengl --disable-video-x11 --disable-pulseaudio --disable-esd --enable-video-opengles --enable-libudev
-# 
-# run make 
-# run sudo make -j 2 install
-# 
-# run sudo rm -f /opt/sdl2/default
-# run sudo ln -s $prefix /opt/sdl2/default
+header "Downloading SDL2"
 
-# SDL image
-# header "Downloading SDL2 Image"
-# sdl2ImageVersion=2.0.1
-# sdl2ImageTar=$srcDir/SDL2_image-${sdl2ImageVersion}.tar.gz
-# sdl2ImageDir=$buildDir/SDL2_image-${sdl2ImageVersion}
-# 
-# run cd $srcDir
-# 
-# if [ ! -e $sdl2ImageTar ]; then
-# 	echo "Downloading SDL2 Image ${sdl2ImageVersion}..."
-# 	run wget https://www.libsdl.org/projects/SDL_image/release/SDL2_image-${sdl2ImageVersion}.tar.gz
-# fi
-# 
-# checkFile $sdl2ImageTar
-# 
-# if [ -e $sdl2ImageDir ]; then
-# 	echo "Removing previously used source..."
-# 	run rm -rfv $sdl2ImageDir
-# fi
-# 
-# run cd $buildDir
-# run tar xvfz $sdl2ImageTar
-# checkDir $sdl2ImageDir
-# run cd $sdl2ImageDir
-# ./configure --prefix=$prefix --host=arm-raspberry-linux-gnueabihf --with-sdl-prefix=$prefix
-# run make -j 2
-# run sudo make install
+run cd $srcDir
 
-# SDL TTF
+if [ ! -e $sdl2Tar ]; then
+	echo "Downloading SDL2-${VERSION}..."
+	run wget http://www.libsdl.org/release/SDL2-${VERSION}.tar.gz
+fi
+
+checkFile $sdl2Tar
+
+if [ -e $sdl2Dir ]; then
+	echo "Removing previously used source..."
+	run rm -rvf $sdl2Dir
+fi
+run cd $buildDir
+run tar xvfz $sdl2Tar
+checkDir $sdl2Dir
+run cd $sdl2Dir
+
+./configure --prefix=$prefix --host=arm-raspberry-linux-gnueabihf --disable-video-opengl --disable-video-x11 --disable-pulseaudio --disable-esd --enable-video-opengles --enable-libudev
+
+run make 
+run sudo make -j 2 install
+
+run sudo rm -f /opt/sdl2/default
+run sudo ln -s $prefix /opt/sdl2/default
+
+SDL image
+header "Downloading SDL2 Image"
+sdl2ImageVersion=2.0.1
+sdl2ImageTar=$srcDir/SDL2_image-${sdl2ImageVersion}.tar.gz
+sdl2ImageDir=$buildDir/SDL2_image-${sdl2ImageVersion}
+
+run cd $srcDir
+
+if [ ! -e $sdl2ImageTar ]; then
+	echo "Downloading SDL2 Image ${sdl2ImageVersion}..."
+	run wget https://www.libsdl.org/projects/SDL_image/release/SDL2_image-${sdl2ImageVersion}.tar.gz
+fi
+
+checkFile $sdl2ImageTar
+
+if [ -e $sdl2ImageDir ]; then
+	echo "Removing previously used source..."
+	run rm -rfv $sdl2ImageDir
+fi
+
+run cd $buildDir
+run tar xvfz $sdl2ImageTar
+checkDir $sdl2ImageDir
+run cd $sdl2ImageDir
+./configure --prefix=$prefix --host=arm-raspberry-linux-gnueabihf --with-sdl-prefix=$prefix
+run make -j 2
+run sudo make install
+
+SDL TTF
 header "Downloading SDL2 TTF"
 sdl2TTFVersion=2.0.14
 sdl2TTFTar=$srcDir/SDL2_ttf-${sdl2TTFVersion}.tar.gz
@@ -123,3 +123,34 @@ run cd $sdl2TTFDir
 ./configure --prefix=$prefix --host=arm-raspberry-linux-gnueabihf --with-sdl-prefix=$prefix
 run make -j 2
 run sudo make install
+
+header "Download SDL GFX"
+sdl2GFXVersion=1.0.1
+sdl2GFXTar=$srcDir/SDL2_gfx-${sdl2GFXVersion}.tar.gz
+sdl2GFXDir=$buildDir/SDL2_gfx-${sdl2GFXVersion}
+
+run cd $srcDir
+
+if [ ! -e $sdl2GFXTar ]; then
+	echo "Downloading SDL2 GFX ${sdl2GFXVersion}..."
+	run wget http://www.ferzkopp.net/Software/SDL2_gfx/SDL2_gfx-${sdl2GFXVersion}.tar.gz
+fi
+
+checkFile $sdl2GFXTar
+
+if [ -e $sdl2GFXDir ]; then
+	echo "Removing previously used source..."
+	run rm -rfv $sdl2GFXDir
+fi
+
+run cd $buildDir
+run tar xvfz $sdl2GFXTar
+checkDir $sdl2GFXDir
+run cd $sdl2GFXDir
+./configure --prefix=$prefix --host=arm-raspberry-linux-gnueabihf --with-sdl-prefix=$prefix --disable-mmx
+run make -j 2
+run sudo make install
+
+header "Installing PySDL2"
+
+run sudo pip2 install PySDL2
