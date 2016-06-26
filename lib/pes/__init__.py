e0e6ec1792bde5066647e38efd485966894acb75
Neil Munday <neilmunday@users.noreply.github.com>
2016-06-26 23:01:07 +0100

    Changed version date.

--- lib/pes/__init__.py
+++ lib/pes/__init__.py
@@ -26,7 +26,7 @@ import sys
 import logging
 
 VERSION_NUMBER = '2.0'
-VERSION_DATE = '2016-06'
+VERSION_DATE = '2016-06-26'
 VERSION_AUTHOR = 'Neil Munday'
 
 baseDir = os.path.abspath('%s%s../../' % (os.path.dirname(os.path.realpath(__file__)), os.sep))
