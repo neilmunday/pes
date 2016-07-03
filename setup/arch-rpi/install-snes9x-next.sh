2b577551e5784427982ac6aff8a24506715f4c29
Neil Munday <neilmunday@users.noreply.github.com>
2016-07-03 21:47:16 +0100

    Added snes9x-next build script for the Raspberry Pi.

---
+++ setup/arch-rpi/install-snes9x-next.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#
+#    This file is part of the Pi Entertainment System (PES).
+#
+#    PES provides an interactive GUI for games console emulators
+#    and is designed to work on the Raspberry Pi.
+#
+#    Copyright (C) 2016 Neil Munday (neil@mundayweb.com)
+#
+#    PES is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    PES is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with PES.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+setupDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ ! -e $setupDir/functions.sh ]; then
+	echo "Error! $setupDir/functions does not exist!"
+	exit 1
+fi
+
+source $setupDir/functions.sh
+
+cd $buildDir
+
+rmSourceDir "snes9x-next"
+
+header "Downloading SNES emulator - snes9x-next"
+
+run git clone git://github.com/libretro/snes9x-next
+checkDir snes9x-next
+cd "snes9x-next"
+checkFile Makefile.libretro
+export CFLAGS="-mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s"
+export CXXFLAGS=$CFLAGS
+run make platform=armv -f Makefile.libretro
+checkFile snes9x_next_libretro.so
+run sudo cp snes9x_next_libretro.so $retroArchCoresDir/
