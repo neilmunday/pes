84f42f6cc0c359f1da8fc7da0079a053f78f5c20
Neil Munday <neilmunday@users.noreply.github.com>
2016-12-16 20:53:04 +0000

    Added MAME 2003 RetroArch core install script for Raspberry Pi 2/3.

---
+++ setup/arch-rpi2/install-mame2003-libretro.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#
+#    This file is part of the Pi Entertainment System (PES).
+#
+#    PES provides an interactive GUI for games console emulators
+#    and is designed to work on the Raspberry Pi.
+#
+#    Copyright (C) 2016 Neil Munday (neil@mundayweb.com)
+#
+#    PES is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    PES is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with PES.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+functions=`realpath $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../common/functions.sh`
+source $functions || exit 1
+
+cd $buildDir
+
+rmSourceDir "mame2003-libretro"
+header "Downloading MAME 2003 emulator"
+
+prj=mame2003-libretro
+
+run git clone https://github.com/libretro/$prj
+checkDir $prj
+cd $prj
+run make -j 2 platform=rpi2 
+checkFile mame2003_libretro.so
+run sudo cp -v mame2003_libretro.so $retroArchCoresDir
