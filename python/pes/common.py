aa8b75b11c90a0435d5f6eda718f23d1eeb2fca1
Neil Munday <neilmunday@users.noreply.github.com>
2017-10-12 23:45:20 +0100

    Added StringMatcher class

--- python/pes/common.py
+++ python/pes/common.py
@@ -21,9 +21,9 @@
 #
 
 from pes import *
-#from Levenshtein import *
 import csv
 import fcntl
+import Levenshtein
 import socket
 import struct
 
@@ -121,3 +121,59 @@ def scaleImage(ix, iy, bx, by):
 		else:
 			sy = by
 	return (int(sx),int(sy))
+
+#
+#	StringMatcher class sourced from https://github.com/ztane/python-Levenshtein/blob/master/StringMatcher.py
+#	Author: Antti Haapala <antti@haapala.name>
+#
+class StringMatcher:
+	"""A SequenceMatcher-like class built on the top of Levenshtein"""
+	def _reset_cache(self):
+		self._ratio = self._distance = None
+		self._opcodes = self._editops = self._matching_blocks = None
+	def __init__(self, seq1='', seq2=''):
+		self._str1, self._str2 = seq1, seq2
+		self._reset_cache()
+	def set_seqs(self, seq1, seq2):
+		self._str1, self._str2 = seq1, seq2
+		self._reset_cache()
+	def set_seq1(self, seq1):
+		self._str1 = seq1
+		self._reset_cache()
+	def set_seq2(self, seq2):
+		self._str2 = seq2
+		self._reset_cache()
+	def get_opcodes(self):
+		if not self._opcodes:
+			if self._editops:
+				self._opcodes = Levenshtein.opcodes(self._editops, self._str1, self._str2)
+			else:
+				self._opcodes = Levenshtein.opcodes(self._str1, self._str2)
+		return self._opcodes
+	def get_editops(self):
+		if not self._editops:
+			if self._opcodes:
+				self._editops = Levenshtein.editops(self._opcodes, self._str1, self._str2)
+			else:
+				self._editops = Levenshtein.editops(self._str1, self._str2)
+		return self._editops
+	def get_matching_blocks(self):
+		if not self._matching_blocks:
+			self._matching_blocks = Levenshtein.matching_blocks(self.get_opcodes(), self._str1, self._str2)
+		return self._matching_blocks
+	def ratio(self):
+		if not self._ratio:
+			self._ratio = Levenshtein.ratio(self._str1, self._str2)
+		return self._ratio
+	def quick_ratio(self):
+		# This is usually quick enough :o)
+		if not self._ratio:
+			self._ratio = Levenshtein.ratio(self._str1, self._str2)
+		return self._ratio
+	def real_quick_ratio(self):
+		len1, len2 = len(self._str1), len(self._str2)
+		return 2.0 * min(len1, len2) / (len1 + len2)
+	def distance(self):
+		if not self._distance:
+			self._distance = Levenshtein.distance(self._str1, self._str2)
+		return self._distance
