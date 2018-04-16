83b7eeba79f0fef262a0dd29fc00681926794e29
Neil Munday <neilmunday@users.noreply.github.com>
2018-04-16 21:04:10 +0100

    Corrected name of bluetooth config file

--- setup/arch-common/customise-os.sh
+++ setup/arch-common/customise-os.sh
@@ -59,7 +59,7 @@ header "Fixing systemd udev service for Bluetooth"
 run sudo sed -r -i "s/^(RestrictAddressFamilies=)(.*?)/\1AF_UNIX AF_NETLINK AF_INET AF_INET6 AF_BLUETOOTH/" /usr/lib/systemd/system/systemd-udevd.service
 
 header "Auto enable Bluetooth (when using Bluez)"
-run sudo sed -r -i "s/AutoEnable=false/AutoEnable=true/" /etc/bluetooth/main.cf
+run sudo sed -r -i "s/AutoEnable=false/AutoEnable=true/" /etc/bluetooth/main.conf
 
 header "Disabling core files in /etc/systemd/system.conf"
 
