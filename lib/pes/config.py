3839b93d981a91a4130ee80e9a281a718a12cb4b
Neil Munday <neilmunday@users.noreply.github.com>
2019-09-08 19:37:11 +0100

    Code refactored by Atom and added desired resolution config settings

--- lib/pes/config.py
+++ lib/pes/config.py
@@ -4,22 +4,22 @@ import os
 from pes import *
 
 class PESConfig(object):
-	
+
 	def __init__(self, configFile):
 		logging.debug("PESConfig.__init__: using %s" % configFile)
-		
+
 		self.__configFile = configFile
 		self.__configParser = ConfigParser.ConfigParser()
 		self.__configParser.read(self.__configFile)
-		
+
 		userHome = os.path.expanduser('~')
-		
+
 		# sanity checks
 		sections = ['settings', 'commands', 'colours', 'font', 'layout']
 		for s in sections:
 			if not self.__configParser.has_section(s):
 				raise Exception("Section %s missing from %s" % (s, self.__configFile))
-			
+
 		# pes settings
 		self.cecEnabled = self.__configParser.getboolean('settings', 'hdmi-cec')
 		self.romsDir = self.__configParser.get('settings', 'romsDir').replace('%%HOME%%', userHome).replace('%%USERDIR%%', userDir)
@@ -28,7 +28,8 @@ class PESConfig(object):
 		self.biosDir = self.__configParser.get('settings', 'biosDir').replace('%%HOME%%', userHome).replace('%%USERDIR%%', userDir)
 		self.kodiDir = self.__configParser.get('settings', 'kodiDir').replace('%%HOME%%', userHome).replace('%%USERDIR%%', userDir)
 		self.screenSaverTimeout = self.__configParser.getint('settings', 'screenSaverTimeout')
-			
+		self.desiredResolution = (self.__configParser.getint('settings', 'desiredScreenWidth'), self.__configParser.getint('settings', 'desiredScreenHeight'))
+
 		# colour settings
 		self.backgroundColour = self.__processColour(self.__configParser.get("colours", "background").split(','))
 		self.menuBackgroundColour = self.__processColour(self.__configParser.get("colours", "menuBackground").split(','))
@@ -38,7 +39,7 @@ class PESConfig(object):
 		self.menuSelectedTextColour = self.__processColour(self.__configParser.get("colours", "menuSelectedText").split(','))
 		self.textColour = self.__processColour(self.__configParser.get("colours", "text").split(','))
 		self.lightBackgroundColour = self.__processColour(self.__configParser.get("colours", "lightBackground").split(','))
-		
+
 		# font settings
 		self.fontFile = self.__configParser.get("font", "fontFile").replace('%%BASE%%', baseDir)
 		self.fontSizes = {}
@@ -48,22 +49,22 @@ class PESConfig(object):
 		self.fontSizes['smallBody'] = self.__configParser.getint("font", "smallBodySize")
 		self.fontSizes['header'] = self.__configParser.getint("font", "headerSize")
 		self.fontSizes['menu'] = self.__configParser.getint("font", "menuSize")
-		
+
 		for f, s in self.fontSizes.iteritems():
 			if s < 1:
 				raise ValueError("Font size %d for \"%s\" is too small!", (s, f))
-		
+
 		# coverart settings
 		self.coverartSize = self.__configParser.getfloat("settings", "coverartSize")
 		self.coverartCacheLen = self.__configParser.getint("settings", "coverartCacheLen")
-		
+
 		# icon settings
 		self.iconCacheLen = self.__configParser.getint("settings", "iconCacheLen")
-		
+
 		# layout
 		self.headerHeight = self.__configParser.getint("layout", "headerHeight")
 		self.menuWidth = self.__configParser.getint("layout", "menuWidth")
-		
+
 		# command settings
 		self.shutdownCommand = self.__configParser.get("commands", "shutdown")
 		self.rebootCommand = self.__configParser.get("commands", "reboot")
@@ -75,7 +76,7 @@ class PESConfig(object):
 		else:
 			logging.warning("PESConfig.init: Kodi command not found, disabling Kodi menu item")
 			self.kodiCommand = None
-		
+
 		# RetroAchievements settings
 		if self.__configParser.has_section("RetroAchievements"):
 			self.retroAchievementsUserName = self.__configParser.get("RetroAchievements", "username")
@@ -85,7 +86,7 @@ class PESConfig(object):
 				self.retroAchievementsHardcore = self.__configParser.getboolean("RetroAchievements", "hardcore")
 			else:
 				self.retroAchievementsHardcore = False
-			
+
 			if len(self.retroAchievementsUserName) == 0 or len(self.retroAchievementsPassword) == 0 or len(self.retroAchievementsApiKey) == 0:
 				self.retroAchievementsUserName = None
 				self.retroAchievementsPassword = None
@@ -94,9 +95,9 @@ class PESConfig(object):
 			self.retroAchievementsUserName = None
 			self.retroAchievementsPassword = None
 			self.retroAchievementsApiKey = None
-			
+
 		logging.debug("PESConfig.__init__: initialised ok!")
-	
+
 	@staticmethod
 	def __processColour(colour):
 		if len(colour) != 3:
@@ -113,6 +114,6 @@ class PESConfig(object):
 		logging.info("Saving PES config to %s" % self.__configFile)
 		with open(self.__configFile, 'wb') as f:
 			self.__configParser.write(f)
-			
+
 	def set(self, section, option, value):
 		self.__configParser.set(section, option, value)
