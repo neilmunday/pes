238c5258b892b459cc80b3430ac19b73167358a5
Neil Munday <neilmunday@users.noreply.github.com>
2019-09-08 18:31:54 +0100

    Updated PES version

--- lib/pes/__init__.py
+++ lib/pes/__init__.py
@@ -25,8 +25,8 @@ import shutil
 import sys
 import logging
 
-VERSION_NUMBER = '2.7'
-VERSION_DATE = '2019-08-06'
+VERSION_NUMBER = '2.8'
+VERSION_DATE = '2019-09-07'
 VERSION_AUTHOR = 'Neil Munday'
 
 baseDir = os.path.abspath('%s%s../../' % (os.path.dirname(os.path.realpath(__file__)), os.sep))
