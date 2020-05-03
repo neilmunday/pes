62ec66592eb70a84aa22d66c7eba3e1e070612be
neilmunday <musclesmunday@hotmail.com>
2020-05-03 16:12:26 +0100

    ffmpeg no longer required

--- setup/arch-rpi4/install-packages.sh
+++ setup/arch-rpi4/install-packages.sh
@@ -37,7 +37,7 @@ run sudo pacman -S bluez-libs bluez-utils \
 	autoconf automake bison byacc flex gcc git cmake make patch pkg-config scons swig vim wget xa \
 	libusb-compat linuxconsole \
 	python2 python2-pip python2-imaging \
-	ffmpeg freetype2 \
+	freetype2 \
 	dosfstools parted \
 	mc \
 	rsync \
